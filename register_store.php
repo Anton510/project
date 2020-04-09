952c2e08b0339976590592acbc3b5055bff47852
Anton510 <51173265+Anton510@users.noreply.github.com>
2020-04-09 20:59:35 +0300

    задание хэширование

--- register_store.php
+++ register_store.php
@@ -1,11 +1,15 @@
 <?php
+$pwd = $_POST['password'];
+
+$hash = password_hash($pwd, PASSWORD_DEFAULT);
+
 //Соединение с базой
 try {
 	$pdo = new PDO("mysql:host=localhost;dbname=test;", "root", "");
 // Подготовка и выполнение запроса
-	$sql = "INSERT INTO register ( name, email, password, Confirm) VALUES ( :name, :email, :password, :Confirm)";
+	$sql = "INSERT INTO register ( name, email, password) VALUES ( :name, :email, :password)";
 	$statement = $pdo->prepare($sql);
-	$statement->execute(['name'=> $_POST['name'], 'email'=> $_POST['email'],'password' => $_POST['password'], 'Confirm' => $_POST['Confirm']]);
+	$statement->execute(['name'=> $_POST['name'], 'email'=> $_POST['email'],'password' => $hash]);
 	header('Location: /index.php');
 }
 catch (PDOException $e)
